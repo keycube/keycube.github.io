1e607ea6a684be3972539a590f64900017ad37f9
Pierre-André RIGAT <rigatpierreandre@gmail.com>
2026-06-11 10:51:21 -0400

    Add mobile device check to scrollbar visibility logic

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,5 +1,7 @@
 const html = document.documentElement;
 const isScrollable = html.scrollHeight > html.clientHeight;
 
-if (isScrollable)
+let isMobileDevice = window.matchMedia("(pointer:coarse) and (max-width: 575.98px)").matches;
+
+if (!isMobileDevice && isScrollable)
     html.style += "scrollbar-gutter: stable;";
