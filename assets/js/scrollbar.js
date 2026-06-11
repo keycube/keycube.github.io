ffa96f7ab84e55a04f1dddc755ae6dbfdb4c62fb
Pierre-André RIGAT <rigatpierreandre@gmail.com>
2026-06-11 15:19:37 -0400

    Refactor CSS for better hand/finger select and update scrollbar logic for mobile devices

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,7 +1,7 @@
 const html = document.documentElement;
-const isScrollable = html.scrollHeight > html.clientHeight;
+let isScrollable = html.scrollHeight > html.clientHeight;
 
-let isMobileDevice = window.matchMedia("(pointer:coarse) and (max-width: 575.98px)").matches;
+let isMobileDevice = window.matchMedia("(pointer:coarse)").matches;
 
 if (!isMobileDevice && isScrollable)
     html.style += "scrollbar-gutter: stable;";
