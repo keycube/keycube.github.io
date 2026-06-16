9e858d26e642c9f9e732e111800232b6c944c17a
Pierre-André RIGAT <rigatpierreandre@gmail.com>
2026-06-16 13:19:55 -0400

    Rename ui.json to ui-lang.json and update related imports; refactor variable declarations for consistency

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,7 +1,7 @@
 const html = document.documentElement;
-let isScrollable = html.scrollHeight > html.clientHeight;
+const isScrollable = html.scrollHeight > html.clientHeight;
 
-let isMobileDevice = window.matchMedia("(pointer:coarse)").matches;
+const isMobileDevice = window.matchMedia("(pointer:coarse)").matches;
 
 if (!isMobileDevice && isScrollable)
     html.style += "scrollbar-gutter: stable;";
