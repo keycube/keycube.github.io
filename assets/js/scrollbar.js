f2dff5fa1cb87cf3a4e6cd7f50f1647812056be0
Pierre-André RIGAT <rigatpierreandre@gmail.com>
2026-06-20 11:26:49 -0400

    Update scrollbar handling and improve button layout for better UI consistency

--- assets/js/scrollbar.js
+++ assets/js/scrollbar.js
@@ -1,7 +1,7 @@
 const html = document.documentElement;
 const isScrollable = html.scrollHeight > html.clientHeight;
 
-const isMobileDevice = window.matchMedia("(pointer:coarse)").matches;
+const isMobileDevice = window.matchMedia("(pointer:coarse) and (width <= 800px)").matches;
 
 if (!isMobileDevice && isScrollable)
     html.style += "scrollbar-gutter: stable;";
