f8e28334ce45fd1350265a4d4bf0291075604413
Pierre-André RIGAT <rigatpierreandre@gmail.com>
2026-07-10 08:52:55 -0400

    Fix for the scrollbar, move the select interface above 3d canvas and remove parenthesis in 3d canvas buttons legend.

--- assets/js/scrollbar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('resize', function() {
-    const html = document.documentElement;
-    let isScrollable = html.scrollHeight > html.clientHeight;
-
-    let isMobileDevice = window.matchMedia("(pointer:coarse) or (width <= 800px)").matches;
-
-    if (!isMobileDevice && isScrollable)
-        html.style += "scrollbar-gutter: stable;";
-});
--- heatmap.markdown
+++ heatmap.markdown
@@ -13,6 +13,4 @@ extra_css:
 
 This 3D render is an interactive visual analysis of the one of the [keycube studies](/research/), with finger reachability mapped directly onto the 3D cube.
 
-{% include heatmap.html %}
-
-{% include heatmap-ui3.html %}
+{% include heatmap2.html %}
